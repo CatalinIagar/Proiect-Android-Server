3a6310a5ebf53abfea34253a4af9792f110092a1
CatalinIagar <cataliniagar2000@gmail.com>
2022-03-16 13:11:02 +0200

    added fragment adapter and created 2 fragments for main activity

--- Client/app/src/main/java/com/example/proiectpaypal/MainActivity.java
+++ Client/app/src/main/java/com/example/proiectpaypal/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.proiectpaypal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPagerFragmentAdapter viewPagerFragmentAdapter;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+
+    private String[] titles = new String[] {"Login", "Signup"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        viewPager2 = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        viewPagerFragmentAdapter = new ViewPagerFragmentAdapter(this);
+
+        viewPager2.setAdapter(viewPagerFragmentAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, ((tab, position) -> tab.setText(titles[position]))).attach();
     }
 }
